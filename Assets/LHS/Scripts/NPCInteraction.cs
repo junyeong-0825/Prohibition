b426173012a1ce44c4e7ca8f11ea82be1243ec87
leehs730 <leehs730@naver.com>
2024-02-11 03:37:47 +0900

    [Fix]NPC 종류별 상호작용 및 컨트롤 초기 수정 중
    일단 종류별로 상호작용 및 컨트롤에 대한 적용을 하는 중
    (일반손님 및 밀주손님 먼저)

--- Assets/LHS/Scripts/NPCInteraction.cs
+++ Assets/LHS/Scripts/NPCInteraction.cs
@@ -2,29 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 일반 손님의 상호작용 스크립트
 public class NPCInteraction : MonoBehaviour
 {
+    // 만족 및 불만족을 표시하는 스프라이트 이미지
     public GameObject satisfiedSprite;
     public GameObject unsatisfiedSprite;
 
+    // 상호작용 제한시간
     public float interactionTimeLimit = 10f;
+    // 주문메(위:테스트용, 아래: enum타입)
     public string orderMenu;
+    public Menu wantedMenu; // 스폰매니저가 랜덤으로 조정할 예정
 
+    // 상호작용 상태가 시작되었다는 것을 알리는 bool값
     [SerializeField] private bool interactionStarted = false;
     public bool InteractionStarted { get { return interactionStarted; } }
+    // 상호작용이 끝났다는 것을 알리는 bool 값
     public bool interactionCompleted = false;
     //public bool InteractionCompleted { get { return interactionCompleted; } }
+    // 상호작용 시간 초기값
     private float interactionTimer = 0f;
 
     private void Start()
     {
         orderMenu = "test";
+        wantedMenu = Menu.Food;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(player_detection && )
+        // 상호작용 시작되었고 상호작용 완료값이 false이면
         if(interactionStarted && !interactionCompleted)
         {
             
@@ -37,6 +46,7 @@ public class NPCInteraction : MonoBehaviour
         }
     }
 
+    // 빈자리 도착시에 상호작용시작 bool 값 변경
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("EmptyChair") && !interactionStarted && !interactionCompleted)
@@ -60,6 +70,25 @@ public class NPCInteraction : MonoBehaviour
         Debug.Log("Interactionstart");
     }
 
+    // enum 비교용 메뉴 비교 메서드
+    public void CompareMenu(Menu playerMenu)
+    {
+        if(!interactionCompleted)
+        {
+            if(playerMenu == wantedMenu)
+            {
+                interactionCompleted = true;
+                satisfiedSprite.SetActive(true);
+                SelfDestroyTargeting();
+            }
+            else
+            {
+                HandleInteractionFailed();
+            }
+        }
+    }
+
+    // 테스트용 플레이어 메뉴 비교 메서드
     public void DeliverMenu(string deliveredMenu)
     {
         if(!interactionCompleted)
@@ -79,6 +108,7 @@ public class NPCInteraction : MonoBehaviour
         }
     }
 
+    // 메뉴 불일치 시의 불만족 메소드
     private void HandleInteractionFailed()
     {
         interactionCompleted = true;
