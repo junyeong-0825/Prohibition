1bdd20caa8e7c88eed9301fd35d7835a651e455c
junyeong-0825 <scv9761@gmail.com>
2024-02-11 14:11:36 +0900

    Penalty Done
    4가지 패널티 구현 완료

--- Assets/KJY_Chara/Scripts/Penalties.cs
+++ Assets/KJY_Chara/Scripts/Penalties.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class Penalties : MonoBehaviour
 {
-    Timer timer = new Timer();
+    private Timer timer;
+    private void Awake()
+    {
+        timer = GameObject.Find("UIManager").GetComponent<Timer>();
+    }
     public void LowLevelGoldPenalty()
     {
-        Inventory.Instance.haveGold += 5;
+        TemporaryDataManager.instance.nowPlayer.Playerinfo.Gold += 5;
+        Inventory.Instance.UpdateUI();
+        Debug.Log("물건 값은 5골드 고정");
     }
     public void HighLevelGoldPenalty()
     {
         //범칙금으로 돈 뜯어감
-        Inventory.Instance.haveGold = Inventory.Instance.haveGold - (Inventory.Instance.haveGold / 5);
+        TemporaryDataManager.instance.nowPlayer.Playerinfo.Gold = TemporaryDataManager.instance.nowPlayer.Playerinfo.Gold - (TemporaryDataManager.instance.nowPlayer.Playerinfo.Gold / 5);
+        Inventory.Instance.UpdateUI();
+        Debug.Log("금주법으로 인한 벌금!");
     }
     public void LowLevelTimePenalty()
     {
         timer.limitTimeSec -= 5f;
+        Debug.Log("시간 5초 감소");
     }
     public void HighLevelTimePenalty()
     {
         timer.limitTimeSec = 0f;
+        Debug.Log("오늘 장사는 접어야돼");
     }
 }
