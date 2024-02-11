db69b906ab5a7eac5c64b6c473cd1a48f52462d3
leehs730 <leehs730@naver.com>
2024-02-11 16:32:53 +0900

    [fix] 스크립트 계속 조정 중
    주석 설명 추가 및 수정 작업 중

--- Assets/LHS/Scripts/NPC.cs
+++ Assets/LHS/Scripts/NPC.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 스크립트 정리 대상
 public class NPC : MonoBehaviour
 {
     [SerializeField] private NPCSO npcSo;
--- Assets/LHS/Scripts/PoliceInteraction.cs
+++ Assets/LHS/Scripts/PoliceInteraction.cs
@@ -11,24 +11,25 @@ public class PoliceInteraction : MonoBehaviour
     // 경찰이 가게 내부가 위장되지 않았을 때를 체크하는 bool 값
     public bool IsCheck = true;
 
+    // 경찰 프리팹이 가게 점검을 실]
     private bool CheckingComplete = false;
     private bool CheckingStarted = false;
 
     // 경찰이 가게 내부에 대기하는데에 걸리는 시간
     public float CheckingTime = 5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if(CheckingStarted && CheckingTime != 0f)
+        if(CheckingStarted && !CheckingComplete)
         {
             // 입구에 들어왔다면 가게 내부를 점검하는 메서드 실행
+            if(CheckingTime > 0f)
+            {
+                // 시간 제한을 5초로 두고 0이 될때까지 메서드 실행
+                CheckingTime -= Time.deltaTime;
+                CheckingRestaurant();
+            }
         }
     }
 
@@ -42,9 +43,23 @@ public class PoliceInteraction : MonoBehaviour
         }
     }
 
+    // 서칭 상태를 시작하는 bool 값 시작
     private void SeachingStart()
     {
         CheckingStarted = true;
+    }
+
+    // 서칭 상태를 종료하는 bool 값들 수정 및 타겟 조정
+    private void SearchingComplete()
+    {
+        CheckingComplete = true;
+        CheckingStarted = false;
+        SelfDestroyTargeting();
+    }
+
+    private void CheckingRestaurant()
+    {
+        // 레스토랑이 위장 상태가 아니면 플레이어한테 패널티 부여 및 스테이지 종료
     }
 
     // 자가 파괴 지정
