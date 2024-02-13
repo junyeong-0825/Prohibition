2985629b3719057cf9661cb1e01a6751ea4ea30f
Jang-Yeongjun <150320634+Jang-Yeongjun@users.noreply.github.com>
2024-02-13 16:07:28 +0900

    DayController 수정
    조금 더 보기 편하게 수정

--- Assets/JangYeongjun/Scripts/Use/Game/DayController.cs
+++ Assets/JangYeongjun/Scripts/Use/Game/DayController.cs
@@ -1,77 +1,116 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System;
+﻿#region NameSpace
+using System.Collections;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
-using Unity.VisualScripting;
+#endregion
+
 
 public class DayController : MonoBehaviour
 {
-    bool dayEnded = false;
-    [SerializeField] Vector3 dayPosition;
-    [SerializeField] Vector3 nightPosition;
-    [SerializeField] Vector3 dayCameraPosition;
-    [SerializeField] Vector3 nightCameraPosition;
+    #region Fields
+    bool IsDay = true;
+    [SerializeField] Vector3 dayPosition, nightPosition, dayCameraPosition, nightCameraPosition;
     [SerializeField] Camera mainCamera;
     [SerializeField] Button dayChangeButton;
     [SerializeField] GameObject Player;
-    Timer timer;
-    NPCSpawner npcSpawner;
+    [SerializeField] Timer timer;
+    [SerializeField] NPCSpawner npcSpawner;
+    #endregion
+
     private void Start()
     {
-        npcSpawner = GameObject.Find("NPCSpawner").GetComponent<NPCSpawner>();
-        timer = GameObject.Find("UIManager").GetComponent<Timer>();
-        GameEvents.OnDayEnd += HandleDayEnd;
-        dayChangeButton.onClick.AddListener(OnButtonClick);
         StartCoroutine("OneDay");
-        
-        //startTime = Time.time;
+    }
+
+    #region Event
+    private void OnEnable()
+    {
+        GameEvents.OnDayEnd += HandleIsday;
+        dayChangeButton.onClick.AddListener(IsDayButtonChange);
     }
     private void OnDisable()
     {
-        GameEvents.OnDayEnd -= HandleDayEnd;
+        GameEvents.OnDayEnd -= HandleIsday;
     }
-    private void HandleDayEnd()
+    #region IsDay Change
+    private void HandleIsday()
     {
-        dayEnded = true;
+        IsDay = false;
     }
 
-    void OnButtonClick()
+    void IsDayButtonChange()
     {
-        dayEnded = false;
+        IsDay = true;
     }
+    #endregion
+    #endregion
 
+    #region OneDay Coroutine
     IEnumerator OneDay()
     {
-        Debug.Log("OneDay시작");
         while (true) // 무한 루프로 낮과 밤 사이클 반복
         {
             Debug.Log("낮");
-            /*
-            밤의 오브젝트 비 활성화
-            NPC Spawner 활성화
-            낮 장면 초기화
-            */
-            timer.limitTimeSec = 240;
-            StartCoroutine(npcSpawner.spawnNPC());
-            Player.transform.position = dayPosition;
-            AudioManager.audioInstance.StopPlayNightSound();
-            AudioManager.audioInstance.PlayMainSound();
-            mainCamera.transform.position = dayCameraPosition;
-            yield return new WaitUntil(() => dayEnded);
+            // 낮 장면 초기화
+            DayTransform();
+            DayAudio();
+            DayNPC();
+
+            yield return new WaitUntil(() => !IsDay);
 
             Debug.Log("밤");
-            /*
-            낮 오브젝트 비 활성화
-            밤 장면 초기화
-            */
-            timer.limitTimeSec = 0;
-            StopCoroutine(npcSpawner.spawnNPC());
-            Player.transform.position = nightPosition;
-            AudioManager.audioInstance.StartPlayNightSound();
-            mainCamera.transform.position = nightCameraPosition;
-            yield return new WaitUntil(() => !dayEnded);
+            // 밤 장면 초기화
+            NightTransform();
+            NightAudio();
+            NightNPC();
+
+            yield return new WaitUntil(() => IsDay);
         }
     }
+    #endregion
+
+    #region Reset Voids
+
+    #region Reset Transform
+    void DayTransform()
+    {
+        Player.transform.position = dayPosition;
+        mainCamera.transform.position = dayCameraPosition;
+    }
+
+    void NightTransform()
+    {
+        Player.transform.position = nightPosition;
+        mainCamera.transform.position = nightCameraPosition;
+    }    
+    
+    #endregion
+
+    #region Reset Audio
+    void DayAudio()
+    {
+        AudioManager.audioInstance.StopPlayNightSound();
+        AudioManager.audioInstance.PlayMainSound();
+    }
+
+    void NightAudio()
+    {
+        AudioManager.audioInstance.StartPlayNightSound();
+    }
+    #endregion
+
+    #region Reset NPC
+    void DayNPC()
+    {
+        timer.limitTimeSec = 240;
+        StartCoroutine(npcSpawner.spawnNPC());
+    }
+    void NightNPC()
+    {
+        timer.limitTimeSec = 0;
+        StopCoroutine(npcSpawner.spawnNPC());
+    }
+    #endregion
+
+    #endregion
 }
