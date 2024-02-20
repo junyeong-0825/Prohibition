85ecae08c387d3afeb8f5003f61e39a092c383fb
leehs730 <leehs730@naver.com>
2024-02-20 10:20:12 +0900

    [Fix] 밤 스테이지 재디자인 및 최신화
    최신화 작업 진행

--- Assets/LHS/Scripts/NPCSpawner.cs
+++ Assets/LHS/Scripts/NPCSpawner.cs
@@ -15,8 +15,8 @@ public class NPCSpawner : MonoBehaviour
     // 오브젝트 스폰과 파괴 위치값
     [SerializeField] private Transform SpawnPositionPrefab;
     [SerializeField] private List<GameObject> TargetPrefabList;
-    public Dictionary<int, bool> EmptySeatCheck = new Dictionary<int, bool>();
-    [SerializeField] private Transform selfDestroyPositionP;
+    public Dictionary<int, bool> EmptySeatCheck = new Dictionary<int, bool>();//
+    [SerializeField] private Transform selfDestroyPositionP;//
 
     //[SerializeField] private GameObject TargetPrefab;
     private Coroutine spawnCoroutine;
@@ -51,23 +51,6 @@ public class NPCSpawner : MonoBehaviour
     // 일반 NPC를 스폰하는 코루틴 메서드
     internal IEnumerator spawnNPC()
     {
-        //// 테스트용
-        //while (true)
-        //{
-        //    if (usedTargetIndex.Count > TargetPrefabList.Count)
-        //    {
-        //        yield break;
-        //    }
-        //    if (usedTargetIndex.Count < TargetPrefabList.Count)
-        //    {
-        //        yield return StartCoroutine(SpawnOnce(guestInterval, guestPrefab, SpawnPositionPrefab));
-        //    }
-        //    else
-        //    {
-        //        yield return null;
-        //    }
-        //}
-
         //실전용
         while (true)
         {
@@ -197,6 +180,25 @@ public class NPCSpawner : MonoBehaviour
     }
     
 }
+
+
+
+//// 테스트용
+//while (true)
+//{
+//    if (usedTargetIndex.Count > TargetPrefabList.Count)
+//    {
+//        yield break;
+//    }
+//    if (usedTargetIndex.Count < TargetPrefabList.Count)
+//    {
+//        yield return StartCoroutine(SpawnOnce(guestInterval, guestPrefab, SpawnPositionPrefab));
+//    }
+//    else
+//    {
+//        yield return null;
+//    }
+//}
 
 //private List<int> usedTargetIndex = new List<int>();
 //public List<int> UsedTargetIndex { get { return usedTargetIndex; } set { usedTargetIndex = value; } }
