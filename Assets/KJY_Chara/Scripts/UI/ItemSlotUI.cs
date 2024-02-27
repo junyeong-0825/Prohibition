f82c9bf65ca5305ddebfcbe26ad051bd16bcd7da
Jang-Yeongjun <150320634+Jang-Yeongjun@users.noreply.github.com>
2024-02-27 14:58:31 +0900

    .
    .

--- Assets/KJY_Chara/Scripts/UI/ItemSlotUI.cs
+++ Assets/KJY_Chara/Scripts/UI/ItemSlotUI.cs
@@ -1,10 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class ItemSlotUI : MonoBehaviour
 {
--- Assets/LHS/Scripts/NPCSpawner.cs
+++ Assets/LHS/Scripts/NPCSpawner.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 public class NPCSpawner : MonoBehaviour
