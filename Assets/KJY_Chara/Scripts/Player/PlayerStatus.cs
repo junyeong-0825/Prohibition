9049087c7f8ca4000e4e803e9c04b50f4971bef2
Jang-Yeongjun <150320634+Jang-Yeongjun@users.noreply.github.com>
2024-02-28 20:04:30 +0900

    기능 추가
    1. 음식 되돌려 놓기
    2. 가게 조기 마감
    3. 결과 판넬에 술, 음식 판매 개수 표시
    4. 카운터에 음식 개수 표

--- Assets/KJY_Chara/Scripts/Player/PlayerStatus.cs
+++ Assets/KJY_Chara/Scripts/Player/PlayerStatus.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -16,11 +17,12 @@ public enum Menu
 public class PlayerStatus : MonoBehaviour
 {
     [SerializeField] internal SpriteRenderer imageSprite;
+    [SerializeField] GameObject alcoholPanel;
+    [SerializeField] TextMeshPro foodCountText;
     public bool isUndercover = false;
     public Menu whatServed = Menu.None;
     private Menu result;
     private GameObject UnderCoverUI;
-    [SerializeField] GameObject alcoholPanel;
 
     public static PlayerStatus instance;
     private void Awake()
@@ -29,43 +31,60 @@ public class PlayerStatus : MonoBehaviour
         UnderCoverUI = GameObject.Find("Emoji");
         UnderCoverUI.SetActive(false);
     }
+    private void Start()
+    {
+        FoodCountUpdate();
+    }
+    void FoodCountUpdate()
+    {
+        PlayerInventory foodInven = DataManager.instance.nowPlayer.inventory.Find(item => item.Name == "Food");
+        if (foodInven != null) { foodCountText.text = $"{foodInven.Quantity}"; }
+        else { foodCountText.text = "0"; }
+    }
     public void IsServed(string menu)
     {
+        Debug.Log("serving");
+        bool success = Enum.TryParse<Menu>(menu, out result);
         if (whatServed == Menu.None)
         {
             Inventory.Instance.ResetUI();
             PlayerInventory existingItem = DataManager.instance.nowPlayer.inventory.Find(invItem => invItem.Name == menu);
             if (existingItem != null)
             {
-                bool success = Enum.TryParse<Menu>(existingItem.Name, out result);
                 if (existingItem.Quantity >= 2)
                 {
-                    if (success)
-                    {
-                        whatServed = result;
-                        existingItem.Quantity--;
-                        Inventory.Instance.UpdateUI();
-                    }
+                    existingItem.Quantity--;
+                    ChangeWhatServed(success, result);
                 }
                 else if (existingItem.Quantity == 1)
                 {
-                    if (success)
-                    {
-                        whatServed = result;
-                        existingItem.Quantity = 0;
-                        DataManager.instance.nowPlayer.inventory.Remove(existingItem);
-                        Inventory.Instance.UpdateUI();
-                    }
+                    existingItem.Quantity = 0;
+                    DataManager.instance.nowPlayer.inventory.Remove(existingItem);
+                    ChangeWhatServed(success, result);
                 }
-
-                if(alcoholPanel.activeSelf)
-                {
-                    alcoholPanel.SetActive(false);
-                }
+                AlcoholPanelFalse();
             }
+
+            if(menu == "Food") FoodCountUpdate();
+
             UpdateSprite(existingItem);
         }
-        
+        else if (success && whatServed == result) { AlcoholPanelFalse(); ResetStatus(); FoodCountUpdate(); }
+    }
+    void ChangeWhatServed(bool sucess, Menu result)
+    {
+        if (sucess)
+        {
+            whatServed = result;
+            Inventory.Instance.UpdateUI();
+        }
+    }
+    void AlcoholPanelFalse()
+    {
+        if (alcoholPanel.activeSelf)
+        {
+            alcoholPanel.SetActive(false);
+        }
     }
 
     void UpdateSprite(PlayerInventory existingItem)
@@ -96,6 +115,37 @@ public class PlayerStatus : MonoBehaviour
         {
             UnderCoverUI.SetActive(false);
             isUndercover = false;
+        }
+    }
+    public void ResetStatus()
+    {
+        if (whatServed != Menu.None)
+        {
+            PlayerInventory existingItem = DataManager.instance.nowPlayer.inventory.Find(invItem => invItem.Name == whatServed.ToString());
+            if (existingItem != null && existingItem.Quantity >= 1)
+            {
+                existingItem.Quantity++;
+                Inventory.Instance.UpdateUI();
+            }
+            else if (existingItem == null)
+            {
+                Item item = DataManager.instance.nowPlayer.items.Find(item => item.Name == whatServed.ToString());
+                PlayerInventory newInventoryItem = new PlayerInventory
+                {
+                    Classification = item.Classification,
+                    Name = item.Name,
+                    Quantity = 1,
+                    PurchasePrice = item.PurchasePrice,
+                    SellingPrice = item.SellingPrice,
+                    RiseScale = item.RiseScale,
+                    spritePath = "Sprites/" + item.Name,
+                    EnhancementValue = 0
+                };
+                DataManager.instance.nowPlayer.inventory.Add(newInventoryItem);
+                Inventory.Instance.UpdateUI();
+            }
+            whatServed = Menu.None;
+            imageSprite.sprite = null;
         }
     }
 }
